fc7ecf99bea980ab34b2d1788099bdd20a748e96
michaeldai1006 <dtc74110@gmail.com>
2019-06-12 16:34:50 -0700

    Add draw triangle

--- TDSwiftShape.playground/Contents.swift
+++ TDSwiftShape.playground/Contents.swift
@@ -17,7 +17,10 @@ class TDLiveViewController : UIViewController {
         TDSwiftShape.drawCircle(onView: backgroundView, atCenter: CGPoint(x: 300.0, y: 350.0), redius: 80, lineWidth: 3, fillColor: UIColor.clear.cgColor, strokeColor: UIColor.red.cgColor)
         
         // Dashed Line
-        TDSwiftShape.drawDashedLine(onView: backgroundView, fromPoint: CGPoint(x: 29.9, y: 218.0), toPoint: CGPoint(x: 302.0, y: 92.0), lineWidth: 3, dashLength: 3, dashGap: 4, lineColor: UIColor.white.cgColor)
+        TDSwiftShape.drawDashedLine(onView: backgroundView, fromPoint: CGPoint(x: 29.9, y: 218.0), toPoint: CGPoint(x: 302.0, y: 92.0), lineWidth: 3, dashLength: 3, dashGap: 4, strokeColor: UIColor.white.cgColor)
+        
+        // Rounded triangle
+        TDSwiftShape.drawTriangle(onView: backgroundView, atPoint: CGPoint(x: 100.0, y: 330.0), width: 80.0, height: 120.0, radius: 6.0, lineWidth: 2.0, strokeColor: UIColor.brown.cgColor, fillColor: UIColor.purple.cgColor, rotateAngle: CGFloat(Double.pi))
         
         // Present background view
         self.view = backgroundView
--- TDSwiftShape.playground/Sources/TDSwiftShape.swift
+++ TDSwiftShape.playground/Sources/TDSwiftShape.swift
@@ -15,15 +15,39 @@ public class TDSwiftShape {
         view.layer.addSublayer(shapeLayer)
     }
     
-    public static func drawDashedLine(onView view: UIView, fromPoint from: CGPoint, toPoint to: CGPoint, lineWidth: CGFloat, dashLength: NSNumber, dashGap: NSNumber, lineColor color: CGColor) {
+    public static func drawDashedLine(onView view: UIView, fromPoint from: CGPoint, toPoint to: CGPoint, lineWidth: CGFloat, dashLength: NSNumber, dashGap: NSNumber, strokeColor: CGColor) {
         let shapeLayer = CAShapeLayer()
         
-        shapeLayer.strokeColor = color
+        shapeLayer.strokeColor = strokeColor
         shapeLayer.lineWidth = lineWidth
         shapeLayer.lineDashPattern = [dashLength, dashGap]
         
         let path = CGMutablePath()
         path.addLines(between: [from, to])
+        shapeLayer.path = path
+        
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    public static func drawTriangle(onView view: UIView, atPoint point: CGPoint, width: CGFloat, height: CGFloat, radius: CGFloat, lineWidth: CGFloat, strokeColor: CGColor, fillColor: CGColor, rotateAngle rotate: CGFloat) {
+        let shapeLayer = CAShapeLayer()
+        
+        shapeLayer.strokeColor = strokeColor
+        shapeLayer.fillColor = fillColor
+        shapeLayer.position = point
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.transform = CATransform3DMakeRotation(rotate, 0.0, 0.0, 1.0)
+        
+        let point1 = CGPoint(x: -width / 2, y: height / 2)
+        let point2 = CGPoint(x: 0, y: -height / 2)
+        let point3 = CGPoint(x: width / 2, y: height / 2)
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: height / 2))
+        path.addArc(tangent1End: point1, tangent2End: point2, radius: radius)
+        path.addArc(tangent1End: point2, tangent2End: point3, radius: radius)
+        path.addArc(tangent1End: point3, tangent2End: point1, radius: radius)
+        path.closeSubpath()
         shapeLayer.path = path
         
         view.layer.addSublayer(shapeLayer)
